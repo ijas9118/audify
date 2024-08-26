81dc9ddcb9c8bebb90a7b33d6870aa646f562ac6
Ijas <ahammedijas.official@gmail.com>
2024-08-26 23:05:39 +0530

    otp done

--- controller/userController.js
+++ controller/userController.js
@@ -1,26 +1,89 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 
-exports.createUser = asyncHandler(async (req, res) => {
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+exports.sendOtp = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   const findUser = await User.findOne({ email });
 
   if (!findUser) {
-    const newUser = new User({
-      firstName,
-      lastName,
-      email,
-      password,
-    });
+    const otp = crypto.randomInt(100000, 999999);
+    const otpExpiry = Date.now() + 5 * 60 * 1000;
 
-    await newUser.save();
+    req.session.otp = otp;
+    req.session.otpExpiry = otpExpiry;
+    req.session.tempUser = { firstName, lastName, email, password };
 
-    return res.status(201).json({
-      message: "User signed up successfully",
-      user: newUser,
-    });
+    const mailOptions = {
+      from: "ahammedijas9118@gmail.com",
+      to: email,
+      subject: "Your OTP for Signup",
+      text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.render("layout", {
+        title: "Verify OTP",
+        header: "partials/header",
+        viewName: "users/verifyOtp",
+        error: null,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error sending OTP" });
+    }
   } else {
     throw new Error("User Already Exists");
+  }
+});
+
+exports.verifyAndSignUp = asyncHandler(async (req, res) => {
+  const { otp } = req.body;
+
+  if (req.session.otp && req.session.otpExpiry > Date.now()) {
+    if (req.session.otp == otp) {
+      const { firstName, lastName, email, password } = req.session.tempUser;
+
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+
+      await newUser.save();
+
+      req.session.otp = null;
+      req.session.otpExpiry = null; 
+      req.session.tempUser = null;
+
+      req.session.user = newUser._id;
+      res.redirect("/");
+    } else {
+      res.render("layout", {
+        title: "Verify OTP",
+        header: "partials/header",
+        viewName: "users/verifyOtp",
+        error: "Invalid OTP",
+      });
+    }
+  } else {
+    res.render("layout", {
+      title: "Verify OTP",
+      header: "partials/header",
+      viewName: "users/verifyOtp",
+      error: "OTP has expired. Please sign up again.",
+    });
   }
 });
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,38 +1,44 @@
 const express = require("express");
 const User = require("../models/userModel");
 const router = express.Router();
-const { createUser, loginUser, logoutUser } = require("../controller/userController");
+const {
+  sendOtp,
+  loginUser,
+  logoutUser,
+  verifyAndSignUp,
+} = require("../controller/userController");
 
 router.get("/", (req, res) => {
   if (!req.session.user) {
-    return res.redirect('/login');
+    return res.redirect("/login");
   }
   res.render("layout", {
     title: "Audify",
-    header: req.session.user ? 'partials/login_header' : 'partials/header',
-    viewName: "users/home"
-  }); 
+    header: req.session.user ? "partials/login_header" : "partials/header",
+    viewName: "users/home",
+  });
 });
 
 router.get("/signup", (req, res) => {
   if (req.session.user) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   res.render("layout", {
     title: "Sign Up",
-    header: 'partials/header',
+    header: "partials/header",
     viewName: "users/signup",
   });
 });
-router.post("/signup", createUser);
+router.post("/signup", sendOtp);
+router.post("/verify-otp", verifyAndSignUp);
 
 router.get("/login", (req, res) => {
   if (req.session.user) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   res.render("layout", {
     title: "Login",
-    header: 'partials/header',
+    header: "partials/header",
     viewName: "users/login",
   });
 });
