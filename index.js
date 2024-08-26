a8ddf1f47b3ba3bf6fa9239af60aa340dc61c2e5
Ijas <ahammedijas.official@gmail.com>
2024-08-26 10:19:34 +0530

    User and admin route setup

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const path = require("node:path");
 const connectDB = require("./config/db");
 require("dotenv").config();
-const userRouter = require("./routes/userRoute");
 const { notFound, errorHandler } = require("./middleware/errorHandler");
+const userRouter = require("./routes/userRoute");
+const adminRouter = require('./routes/adminRoute');
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 connectDB();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/user/", userRouter);
+app.use("/", userRouter);
+app.use('/admin', adminRouter);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controller/userController");
 
-router.post("/register", createUser);
+router.get('/', (req, res) => {
+  res.send('Helloooo')
+})
+router.get('/signup', (req, res) => {
+  res.render('users/signup', { title: 'Sign Up' });
+});
+router.post("/signup", createUser);
 
 module.exports = router;
